06d83bddc51bd1aebdd02b031b5576c2f57709d6
osamaraed53 <osamaraed53@gmail.com>
2023-10-01 14:50:49 +0300

    new

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # JS-exesrsise
+
+
+Q1: Evaluate each of the following Javascript expressions and show the value
+1. ‐9*3         
+2. “value is “ + 50   
+3. 17 % 5         
+4. 5 % 17        
+5. 5/10         
+6. (4 == 4)        
+7. (4 != 5)  
+8. (7 <= 8) 
+9. Math.ceil(x) ‐ Math.floor(x) 
+
+Q2 : Read a number (using prompt) and display it using alert
+
+Q3 : Read in two numbers and display them in ascending order.
+
+Q4: Read in two numbers and display the larger
+
+Q5: Read two numbers and display their sum.  What problem did you encounter?
+
+Q6: If you have a “num” variable,  Your task is to print:
+
+- ONE, if it is equal to 1 .
+- TWO, if it is equal to 2.
+- THREE, if it is equal to 3 .
+- FOUR, if it is equal to 4.
+- FIVE, if it is equal to 5.
+- SIX, if it is equal to 6 .
+- SEVEN, if it is equal to 7.
+- EIGHT, if it is equal to 8.
+- NINE, if it is equal to 9.
+- PLEASE TRY AGAIN, if there is none of the above.
---
+++ solution.js
@@ -0,0 +1,77 @@
+
+
+// Q1
+
+//1- -27
+//2- valueis50
+//3- 2
+//4 - 0.5
+//5-  true
+//6- true
+//7- true
+//8- Math.floor(1.2) - Math.ceil(1.2) = -1
+
+
+
+// Q2
+
+// let num = prompt("Enter num please ");
+// alert(`your num is ${num}`)
+
+// Q3
+
+// let num1 = prompt("Enter num1 please ");
+// let num2 = prompt("Enter num2 please ");
+// if(num1 >= num2){
+//     console.log(`${num1} , ${num2}`);
+// }else{
+//     console.log(`${num2} , ${num1}`);
+// }
+
+// Q4
+// let num1 = prompt("Enter num1 please ");
+// let num2 = prompt("Enter num2 please ");
+// console.log(Math.max(num1,num2));
+
+// Q5
+// let num1 = prompt("Enter num1 please ");
+// let num2 = prompt("Enter num2 please ");
+// console.log(Number(num1)+Number(num2)); 
+// the prompt take the numbers as string 
+
+let num = prompt("Enter num please : ")
+
+switch(Number(num)){
+    case 1:
+        console.log("ONE");
+        break;
+    case 2:
+        console.log("TWO");
+        break;
+    case 3:
+        console.log("THREE");
+        break;
+    case 4:
+        console.log("FOUR");
+        break;
+    case 5:
+        console.log("FIVE");
+        break;
+    case 6:
+        console.log("SIX");
+        break;
+    case 7:
+        console.log("SEVEN");
+        break;
+    case 8:
+        console.log("EIGHT");
+        break;
+    case 9:
+        console.log("NINE");
+        break;
+    default:
+        console.log("PLEASE TRY AGAIN");
+
+}
+
+
