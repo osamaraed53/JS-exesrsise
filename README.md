2ded43fb8da780349c91ab5276d232c32830689b
osamaraed53 <osamaraed53@gmail.com>
2023-10-02 21:55:29 +0300

    new

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ Q5: Read two numbers and display their sum.  What problem did you encounter?
 
 Q6: If you have a “num” variable,  Your task is to print:
 
-- ONE, if it is equal to 1 .
+- ONE, if it is equal to 1 .cod
 - TWO, if it is equal to 2.
 - THREE, if it is equal to 3 .
 - FOUR, if it is equal to 4.
@@ -32,3 +32,22 @@ Q6: If you have a “num” variable,  Your task is to print:
 - EIGHT, if it is equal to 8.
 - NINE, if it is equal to 9.
 - PLEASE TRY AGAIN, if there is none of the above.
+
+
+Q7: Use a loop to display the numbers 0 through 5, each in a separate alert window.
+
+Q8: Use a loop to display the numbers 0 through 5 in a single alert window
+
+Q9: Use a loop to display the numbers in the range 0…20 that are multiples of 3. 
+
+Q10: Prompt the user for a number in the range 0…100.  If the number is out of range, display an error message and prompt again until a valid number is entered.  Assume the user enters a number each time
+
+
+Q11: Repeat previous exercise, but this time allow for the possibility that the user enters something that is not a number.  Hint: the built in function isNaN(x) returns true if x is not a number.  It returns false if x is a number.  Sort of backwards, but that's what's available.
+
+
+Q12: Prompt for an integer, then display the sum of the integers from 0 through the number entered.  For example, if you enter 10, then display 55 which is the sum of   0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+
+
+Q13: Prompt for an integer, then display the average of the integers from 0 through the number entered.  For example, if you enter 10, then display 5 which is the average of   (0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)/11
+
--- solution.js
+++ solution.js
@@ -39,39 +39,96 @@
 // console.log(Number(num1)+Number(num2)); 
 // the prompt take the numbers as string 
 
-let num = prompt("Enter num please : ")
+// let num = prompt("Enter num please : ")
 
-switch(Number(num)){
-    case 1:
-        console.log("ONE");
-        break;
-    case 2:
-        console.log("TWO");
-        break;
-    case 3:
-        console.log("THREE");
-        break;
-    case 4:
-        console.log("FOUR");
-        break;
-    case 5:
-        console.log("FIVE");
-        break;
-    case 6:
-        console.log("SIX");
-        break;
-    case 7:
-        console.log("SEVEN");
-        break;
-    case 8:
-        console.log("EIGHT");
-        break;
-    case 9:
-        console.log("NINE");
-        break;
-    default:
-        console.log("PLEASE TRY AGAIN");
+// switch(Number(num)){
+//     case 1:
+//         console.log("ONE");
+//         break;
+//     case 2:
+//         console.log("TWO");
+//         break;
+//     case 3:
+//         console.log("THREE");
+//         break;
+//     case 4:
+//         console.log("FOUR");
+//         break;
+//     case 5:
+//         console.log("FIVE");
+//         break;
+//     case 6:
+//         console.log("SIX");
+//         break;
+//     case 7:
+//         console.log("SEVEN");
+//         break;
+//     case 8:
+//         console.log("EIGHT");
+//         break;
+//     case 9:
+//         console.log("NINE");
+//         break;
+//     default:
+//         console.log("PLEASE TRY AGAIN");
 
-}
+// }
+
+
+// Q7
+// for(let i=0 ; i<=5; i++){
+//     window.alert(i);
+// }
+
+
+
+// Q8
+// let str = " ";
+// for(let i = 0 ; i<=5;i++){
+//     str += (" "+ i); 
+// }
+// alert(str);
+
+
+
+// Q9
+// for(let i =3; i<20 ;i+=3){
+//     console.log(i);
+// }
+
+// Q10
+// let num= prompt('enter num between 100 and 0');
+// while(num > 100 || num<0){
+//     alert('Stick to the required number');
+//     num =prompt('enter num between 100 and 0');
+// }
+
+
+// Q11
+// let new_str= '';
+// let str= prompt('enter str');
+// for(let i =str.length-1 ; i>=0 ; i--){
+//     new_str+=str.charAt(i);
+// }
+// alert(new_str);
+
+
+// // Q12
+// let y=prompt("enter number");
+// function sum(x){
+//     if(x<=0)
+//         return 0; 
+//     return sum(x-1)+x;
+// }
+// let total = sum(Number(y));
+// alert(total);
+
+// // Q13
+// alert(total/(Number(y)+1));
+
+
+
+
+
 
 
